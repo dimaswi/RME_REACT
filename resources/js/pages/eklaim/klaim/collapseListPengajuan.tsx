912827c51adf2e9d7eed38bcfdd36a09e53b4c1f
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-06-24 22:58:31 +0700

    Update collapseListPengajuan.tsx

--- resources/js/pages/eklaim/klaim/collapseListPengajuan.tsx
+++ resources/js/pages/eklaim/klaim/collapseListPengajuan.tsx
@@ -364,6 +364,7 @@ export default function PengajuanKlaimCollapse({ item, formatTanggal, getStatusB
             if (item.edit == 1) {
                 const klaimRes = await axios.get(`/eklaim/get/pengajuan-klaim/${item.id}`);
                 const klaimData = klaimRes.data;
+                console.log('Klaim Data:', klaimData);
                 // Kodisi jika data klaim sudah ada
                 if (klaimData && klaimData.klaimData) {
                     setSistole(klaimData.klaimData.sistole || '');
@@ -508,6 +509,7 @@ export default function PengajuanKlaimCollapse({ item, formatTanggal, getStatusB
 
             if (item.edit == 0) {
                 const response = await axios.get(`/eklaim/get/pengajuan-klaim/${item.id}`);
+                console.log('Response Kunjungan:', response.data);
                 setJenisPerawatan(
                     response.data.kunjungan.jenis_perawatan === 'Rawat Jalan' ? '2' : response.data.kunjungan.jenis_perawatan === 'IGD' ? '3' : '1',
                 );
