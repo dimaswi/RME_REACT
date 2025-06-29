25c47abe795edbea2c37c0f1c97684b8f9eea72b
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-06-30 04:14:51 +0700

    Update BridgeDataController.php

--- app/Http/Controllers/Eklaim/BridgeDataController.php
+++ app/Http/Controllers/Eklaim/BridgeDataController.php
@@ -1290,6 +1290,7 @@ class BridgeDataController extends Controller
                     }),
                 ];
             })
+            ->groupBy('kunjungan_id') // Group by kunjungan_id
             ->values();
         // dd($dataLaboratorium);
 
