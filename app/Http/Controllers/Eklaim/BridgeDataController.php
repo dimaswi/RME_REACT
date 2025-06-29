07cb40f5f7f9d6c68057d550434c84122544823e
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-06-30 04:35:38 +0700

    Update BridgeDataController.php

--- app/Http/Controllers/Eklaim/BridgeDataController.php
+++ app/Http/Controllers/Eklaim/BridgeDataController.php
@@ -1293,8 +1293,6 @@ class BridgeDataController extends Controller
             ->groupBy('kunjungan_id') // Group by kunjungan_id
             ->values();
 
-            dd($dataLaboratorium);
-
         $imagePath = public_path('images/kop.png'); // Path ke gambar di folder public
         if (!file_exists($imagePath)) {
             throw new \Exception("Gambar tidak ditemukan di path: $imagePath");
